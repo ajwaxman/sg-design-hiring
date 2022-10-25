d1eeef293f23eb5ebf24bb3d1ef48bfcfe1a90d9
Adam Waxman <adamw@seatgeek.com>
2022-10-25 19:39:16 -0400

    Simple animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Image from "next/image"
 import Head from "next/head";
+import { motion } from "framer-motion"
 
 const incentives = [
     {
@@ -62,7 +63,11 @@ const Home: NextPage = () => {
                     <p className="text-2xl text-silver-dark">Help the world experience more live.</p>
                 </div> */}
                 <div className="bg-sg-gold-light-08 mx-auto flex min-content flex-col items-center justify-center">
-                    <div className="mx-auto max-w-6xl py-24 sm:px-2 sm:py-32 lg:px-4">
+                    <motion.div 
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                        className="mx-auto max-w-6xl py-24 sm:px-2 sm:py-32 lg:px-4">
                         <h2 className="text-3xl width-full px-8 text-left font-bold leading-normal md:text-[1.75rem] pb-16">
                             Our Values
                         </h2>
@@ -75,7 +80,8 @@ const Home: NextPage = () => {
                                         <Image width={100} height={100} src={incentive.imageSrc} alt=""/>
                                     </div> 
                                 </div> */}
-                                    <div className="sm:flex-shrink-0">
+                                    <div 
+                                        className="sm:flex-shrink-0">
                                         <div className="bg-white w-16 h-16 sm:w-20 sm:h-20 drop-shadow-sm rounded-2xl flex items-center">
                                             <div className="text-3xl sm:text-4xl mx-auto">{incentive.emoji}</div>
                                         </div>
@@ -87,52 +93,79 @@ const Home: NextPage = () => {
                                 </div>
                             ))}
                         </div>
-                    </div>
+                    </motion.div>
+                </div >
+                <div className="mx-auto justify-center p-4 py-36">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                        className="text-center flex flex-col items-center"
+                    >
+                        <h1 className="text-4xl font-bold leading-normal text-gray-dark md:text-[3rem]">
+                            Find the best deals
+                        </h1>
+                        <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-gray-midtone mb-8 mt-2">
+                            Use deal score to save time and money.
+                        </p>
+                        <Image width={525} height={981} src="/images/event.png" alt="" />
+                    </motion.div>
                 </div>
-                <div className="mx-auto flex flex-col items-center justify-center p-4 py-36">
-                    <h1 className="text-4xl font-bold leading-normal text-gray-dark md:text-[3rem]">
-                        Find the best deals
-                    </h1>
-                    <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-gray-midtone mb-8 mt-2">
-                        Use deal score to save time and money.
-                    </p>
-                    <Image width={525} height={981} src="/images/event.png" alt=""/>
+                <div className="mx-auto bg-gray-dark justify-center p-4 py-36">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                        className="text-center flex flex-col items-center"
+                    >
+                        <h1 className="text-4xl font-bold leading-normal text-white md:text-[3rem]">
+                            Check out your view
+                        </h1>
+                        <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-silver-dark/70 mb-8 mt-2">
+                            Know exactly what to expect.
+                        </p>
+                        <Image width={525} height={981} src="/images/vfs.png" alt="" />
+                    </motion.div>
                 </div>
-                <div className="mx-auto bg-gray-dark flex flex-col items-center justify-center p-4 py-36">
-                    <h1 className="text-4xl font-bold leading-normal text-white md:text-[3rem]">
-                        Check out your view
-                    </h1>
-                    <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-silver-dark/70 mb-8 mt-2">
-                        Know exactly what to expect.
-                    </p>
-                    <Image width={525} height={981} src="/images/vfs.png" alt="" />
-                </div>
-                <div className="mx-auto flex flex-col items-center justify-center p-4 py-36">
-                    <h1 className="text-3xl font-bold leading-normal text-gray-dark md:text-[3rem]">
-                        Get in with your phone
-                    </h1>
-                    <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-gray-midtone mb-8 mt-2">
-                        Manage and scan tickets with ease.
-                    </p>
-                    <Image width={525} height={981} src="/images/scanning.png" alt="" />
-                </div>                            
+                <div className="mx-auto justify-center p-4 py-36">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                        className="text-center flex flex-col items-center"
+                    >
+                        <h1 className="text-4xl font-bold leading-normal text-gray-dark md:text-[3rem]">
+                            Manage and scan tickets with ease.
+                        </h1>
+                        <p className="text-xl md:text-2xl md:leading-normal max-w-md text-center text-gray-midtone mb-8 mt-2">
+                            Manage and scan tickets with ease.
+                        </p>
+                        <Image width={525} height={981} src="/images/scanning.png" alt="" />
+                    </motion.div>
+                </div>                          
                 <div className="mx-auto bg-sg-gold-light-24 flex min-h-[85vh] flex-col items-center justify-center p-4">
-                    <h1 className="text-5xl font-bold leading-normal text-gray-dark md:text-[5rem]">
-                        Let&#39;s Go
-                    </h1>
-                    <div className="rounded-md shadow mt-8">
-                        <a
-                            href="#"
-                            className="flex w-full items-center justify-center rounded-md border 
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                        >
+                        <h1 className="text-5xl font-bold leading-normal text-gray-dark md:text-[5rem]">
+                            Let&#39;s Go
+                        </h1>
+                        <div className="rounded-md shadow mt-8">
+                            <a
+                                href="#"
+                                className="flex w-full items-center justify-center rounded-md border 
                                     transition-all duration-300
                                     bg-black
                                     hover:bg-black/70
                                     active:bg-black
                                     px-8 py-3 text-base font-medium text-white md:py-4 md:px-10 md:text-lg"
-                        >
-                            View openings
-                        </a>
-                    </div>
+                            >
+                                View openings
+                            </a>
+                        </div>
+                    </motion.div>
                 </div>                                   
             </main>
         </>
