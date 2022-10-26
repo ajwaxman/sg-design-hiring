40a01c263d97a14d6df93b3715ec4ee1c46b4da6
Adam Waxman <awaxman11@gmail.com>
2022-10-25 23:10:58 -0400

    Update meta tag descriptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,8 @@ const Home: NextPage = () => {
         <>
             <Head>
                 <title>SeatGeek Design</title>
-                <meta name="description" content="Come join the SeatGeek design team!" />
                 <meta property="og:title" content="Come join the SeatGeek design team!" />
+                <meta name="description" content="Ticketing sucks. Join us in our mission to make it better." />
                 <meta property="og:image" content="https://sg-design-hiring.vercel.app/images/portland.jpg" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
